74210b75207ad2f67e48de3bf910eb74586d3e50
arunava-webdev4u <arunava52486@gmail.com>
2021-05-17 00:51:07 +0530

    Themes added

--- Digital_Clock/JS/JS.js
+++ Digital_Clock/JS/JS.js
@@ -1,3 +1,4 @@
+// clock time changing code
 setInterval(function(){
     var date = new Date();
     var hours = date.getHours();    //0-23
@@ -19,3 +20,48 @@ setInterval(function(){
     var clock = document.querySelector("#clock");
     clock.innerText = clockTime;
 }, 1000);
+
+
+// color theme changing code
+var colorList = ["rgb(255, 255, 255)", "rgb(3, 220, 253)", "rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(161, 3, 253)", "rgb(255, 160, 122)", "rgb(93,138,168)", "rgb(253, 3, 95 )", "rgb(218, 247, 166)"];
+var theme = document.querySelector('#theme');
+let i = 0;
+var h1 = document.querySelector('h1');
+var themeButton = document.querySelector('#theme');
+var clock = document.querySelector('#clock');
+
+theme.addEventListener('click', () => {
+    i = i + 1;
+    if (i === 8)
+        i = 0;
+    // h1
+    h1.style.color = colorList[i];
+    h1.style.borderColor = colorList[i];
+
+    //theme button
+    themeButton.style.backgroundColor = colorList[i];
+    themeButton.style.color = "black";
+    themeButton.style.borderColor = colorList[i];
+    themeButton.addEventListener('mouseenter', () =>{
+        themeButton.style.backgroundColor = colorList[i];
+        themeButton.style.color = "black";
+    });
+    themeButton.addEventListener('mouseleave', () => {
+        themeButton.style.backgroundColor = "black";
+        themeButton.style.color = colorList[i];
+    });
+
+    // clock
+    clock.style.color = colorList[i];
+    clock.style.borderColor = colorList[i];
+    clock.addEventListener('mouseenter', () => {
+        clock.style.backgroundColor = colorList[i];
+        clock.style.color = "black";
+        clock.style.boxShadow = `0 0px 20px ${colorList[i]}`;
+    });
+    clock.addEventListener('mouseleave', () => {
+        clock.style.backgroundColor = "black";
+        clock.style.color = colorList[i];
+        clock.style.boxShadow = "0 0px 20px black";
+    });
+});
