951953a0f7a5ce066a631a038381e849f813e716
arunava-webdev4u <arunava52486@gmail.com>
2022-07-04 15:37:11 +0530

    Project initiated

---
+++ Birthday Remainder App/birthday-remainder/src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import './App.css'
+
+function App() {
+	return (
+        <div className="App">
+			<header className="App-header">
+                Birthday Remainder App
+			</header>
+            <div className="body">
+                <div className="display">
+                    
+                </div>
+                <div className="add">
+                    <div className="add-title">
+                        Add more Birthdays ...
+                    </div>
+                    <div classNam="add-input">
+                        <input type="text"></input>
+                        <button>Add</button>
+                    </div>
+                </div>
+            </div>
+		</div>
+	);
+}
+
+export default App;
